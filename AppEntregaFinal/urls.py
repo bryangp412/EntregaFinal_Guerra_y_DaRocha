95fca2c65d32952c90d6a111a032d708e90091d6
Bryan <bryanguerraperales@gmail.com>
2022-08-02 17:48:22 -0500

    agregue templates , models y views

--- AppEntregaFinal/urls.py
+++ AppEntregaFinal/urls.py
@@ -1,7 +1,16 @@
 from django.urls import path
-from .views import una_vista,mi_template
+from .views import contacto, index, cartelera, locales, nosotros, productos, privacidad, terminos
 
 urlpatterns = [
-    path('', una_vista),
-    path('mi_template', mi_template)
+    path("", index, name="index"),
+    path('cartelera', cartelera),
+    path('locales', locales),
+    path('productos', productos),
+    path("principal", index),
+    path("privacidad", privacidad, name="privacidad"),
+    path("terminos", terminos, name="terminos"),
+    path("nosotros", nosotros, name="nosotros"),
+    path("contacto", contacto, name="contacto")
+    
+    
 ]
--- AppEntregaFinal/views.py
+++ AppEntregaFinal/views.py
@@ -1,12 +1,38 @@
 from re import template
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
+
+from AppEntregaFinal.models import Prueba
 # Create your views here.
 
-def una_vista(request):
-    return HttpResponse('<h1>Una vista</h1>')
+def index(request):
+    return render(request, 'index.html')
 
-def mi_template(request):
-    template = loader.get_template('index.html')
-    render = template.render({})
+def cartelera(request):
+    template = loader.get_template('cartelera.html')
+    
+    prueba1= Prueba(nombre='Pepe')
+    prueba2= Prueba(nombre='Pepa')
+    prueba3= Prueba(nombre='Pepq')
+    
+    render = template.render({'lista_objeto':[prueba1,prueba2,prueba3]})
     return HttpResponse(render)
+
+def locales(request):
+    return render(request, 'locales.html')
+
+def productos(request):
+    return render(request, 'productos.html')
+
+def privacidad(request):
+    return render(request, 'privacidad.html')
+
+def terminos(request):
+    return render(request, 'terminos.html')
+
+def nosotros(request):
+    return render(request, 'nosotros.html')
+
+def contacto(request):
+    return render(request, 'contacto.html')
