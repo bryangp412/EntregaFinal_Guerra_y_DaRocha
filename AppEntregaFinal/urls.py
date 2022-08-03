c7ede04a77e8cfe545007a010f900f792430b772
Bryan <bryanguerraperales@gmail.com>
2022-08-02 19:17:51 -0500

    Agregue más templates

--- AppEntregaFinal/urls.py
+++ AppEntregaFinal/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import agregarpeli, contacto, index, cartelera, locales, nosotros, productos, privacidad, terminos
+from .views import agregarlocal, agregarpeli, agregarprodu, contacto, index, cartelera, locales, nosotros, productos, privacidad, terminos
 
 urlpatterns = [
     path("", index, name="index"),
@@ -11,7 +11,10 @@ urlpatterns = [
     path("terminos", terminos, name="terminos"),
     path("nosotros", nosotros, name="nosotros"),
     path("contacto", contacto, name="contacto"),
-    path("agregarpeli", agregarpeli, name="agregarpeli")
+    path("agregarpeli", agregarpeli, name="agregarpeli"),
+    path("agregarprodu", agregarprodu, name="agregarprodu"),
+    path("agregarlocal", agregarlocal, name="agregarlocal")
+    
     
     
 ]
--- AppEntregaFinal/views.py
+++ AppEntregaFinal/views.py
@@ -12,18 +12,31 @@ def index(request):
 def cartelera(request):
     template = loader.get_template('cartelera.html')
     
-    prueba1= Prueba(nombre='Pepe')
-    prueba2= Prueba(nombre='Pepa')
-    prueba3= Prueba(nombre='Pepq')
+    prueba1= Pelicula(nombre='Pepe')
+    prueba2= Pelicula(nombre='Pepa')
+    prueba3= Pelicula(nombre='Pepq')
     
     render = template.render({'lista_objeto':[prueba1,prueba2,prueba3]})
     return HttpResponse(render)
 
 def locales(request):
-    return render(request, 'locales.html')
+    template = loader.get_template('locales.html')
+
+    prueba1= Locales(nombre='Pepe')
+    prueba2= Locales(nombre='Pepa')
+    prueba3= Locales(nombre='Pepq')
+    
+    render = template.render({'lista_objeto':[prueba1,prueba2,prueba3]})
+    return HttpResponse(render)
 
 def productos(request):
-    return render(request, 'productos.html')
+    template = loader.get_template('productos.html')
+    prueba1= Locales(nombre='Pepe')
+    prueba2= Locales(nombre='Pepa')
+    prueba3= Locales(nombre='Pepq')
+    
+    render = template.render({'lista_objeto':[prueba1,prueba2,prueba3]})
+    return HttpResponse(render)
 
 def privacidad(request):
     return render(request, 'privacidad.html')
@@ -38,3 +51,7 @@ def contacto(request):
     return render(request, 'contacto.html')
 def agregarpeli(request):
     return render(request, 'agregarpeli.html')
+def agregarlocal(request):
+    return render(request, 'agregarlocal.html')
+def agregarprodu(request):
+    return render(request, 'agregarprodu.html')
